9f055648eabfe0d402594e36b6864e953d190f63
Lachlan Donald <lachlan@buildkite.com>
2017-08-17 11:27:57 +1000

    De-make packer script

--- .buildkite/steps/packer.sh
+++ .buildkite/steps/packer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-docker run  -e AWS_DEFAULT_REGION  -e AWS_ACCESS_KEY_ID \
+docker run -e AWS_DEFAULT_REGION  -e AWS_ACCESS_KEY_ID \
 	-e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN \
-	-v ${HOME}/.aws:/root/.aws \
-	--rm -v "$(PWD):/src" -w /src/packer hashicorp/packer:light \
+	-v "${HOME}/.aws:/root/.aws" \
+	--rm -v "${PWD}:/src" -w /src/packer hashicorp/packer:light \
 		build buildkite-ami.json | tee packer.output
