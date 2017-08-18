54bec1cde645238a8546369cf91a9987542970b3
Lachlan Donald <lachlan@buildkite.com>
2017-08-18 13:02:36 +1000

    Add a test step and script

--- .buildkite/steps/packer.sh
+++ .buildkite/steps/packer.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "--- :packer: Building Windows AMI"
 docker run -e AWS_DEFAULT_REGION  -e AWS_ACCESS_KEY_ID \
-	-e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN \
-	-v "${HOME}/.aws:/root/.aws" \
-	--rm -v "${PWD}:/src" -w /src/packer hashicorp/packer:light \
-		build buildkite-ami.json | tee packer.output
+  -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN \
+  -v "${HOME}/.aws:/root/.aws" \
+  --rm -v "${PWD}:/src" -w /src/packer hashicorp/packer:light \
+    build buildkite-ami.json | tee packer.output
+
+image_id=$(grep -Eo "us-east-1: (ami-.+)$" "packer.output" | awk '{print $2}')
+echo "AMI for us-east-1 is $image_id"
+
+buildkite-agent meta-data set image_id "$image_id"
---
+++ .buildkite/steps/test.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# shellcheck disable=SC1117
+set -eu
+
+vpc_id=$(aws ec2 describe-vpcs --filters "Name=isDefault,Values=true" --query "Vpcs[0].VpcId" --output text)
+subnets=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc_id" --query "Subnets[*].[SubnetId,AvailabilityZone]" --output text)
+subnet_ids=$(awk '{print $1}' <<< "$subnets" | tr ' ' ',' | tr '\n' ',' | sed 's/,$//')
+az_ids=$(awk '{print $2}' <<< "$subnets" | tr ' ' ',' | tr '\n' ',' | sed 's/,$//')
+
+image_id=$(buildkite-agent meta-data get image_id)
+echo "Using AMI $image_id"
+
+cat << EOF > config.json
+[
+  {
+    "ParameterKey": "BuildkiteOrgSlug",
+    "ParameterValue": "$BUILDKITE_AWS_STACK_ORG_SLUG"
+  },
+  {
+    "ParameterKey": "BuildkiteAgentToken",
+    "ParameterValue": "$BUILDKITE_AWS_STACK_AGENT_TOKEN"
+  },
+  {
+    "ParameterKey": "BuildkiteApiAccessToken",
+    "ParameterValue": "$BUILDKITE_AWS_STACK_API_TOKEN"
+  },
+  {
+    "ParameterKey": "BuildkiteQueue",
+    "ParameterValue": "${AWS_STACK_QUEUE_NAME}"
+  },
+  {
+    "ParameterKey": "KeyName",
+    "ParameterValue": "${AWS_KEYPAIR:-default}"
+  },
+  {
+    "ParameterKey": "ImageId",
+    "ParameterValue": "${image_id}"
+  },
+  {
+    "ParameterKey": "VpcId",
+    "ParameterValue": "${vpc_id}"
+  },
+  {
+    "ParameterKey": "Subnets",
+    "ParameterValue": "${subnet_ids}"
+  },
+  {
+    "ParameterKey": "AvailabilityZones",
+    "ParameterValue": "${az_ids}"
+  },
+  {
+    "ParameterKey": "MaxSize",
+    "ParameterValue": "1"
+  }
+]
+EOF
+
+version=$(git describe --tags --candidates=1)
+
+echo "--- Creating stack ${AWS_STACK_NAME} ($version)"
+aws cloudformation create-stack \
+  --output text \
+  --stack-name "${AWS_STACK_NAME}" \
+  --disable-rollback \
+  --template-body "file://${PWD}/pipeline.yml" \
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+  --parameters "$(cat config.json)"
+
+echo "--- Waiting for stack to complete"
+aws cloudformation wait stack-create-complete --stack-name "${AWS_STACK_NAME}"
